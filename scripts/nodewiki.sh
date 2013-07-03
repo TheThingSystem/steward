4309ffe0982e08ed4d41e3e896e5056a0cb0b742
Alasdair Allan <alasdair@babilim.co.uk>
2013-07-03 13:08:34 +0100

    Fixed nodewiki script.

--- scripts/nodewiki.sh
+++ scripts/nodewiki.sh
@@ -2,4 +2,10 @@
 
 cd ../nodewiki || exit 1
 
-nodewiki.js --local --port 8008
+path_to_executable=$(which nodewiki)
+if [ -x "$path_to_executable" ] ; then
+    nodewiki --local --port 8008
+else
+    nodewiki.js --local --port 8008
+fi
+
