d733f577b32e90124944cd13f9f56041ed507f60
Elijah Insua <tmpvar@gmail.com>
2013-08-08 16:35:34 -0700

    make nest `perform` operations work
    includes:
    
    * away - on/off
    * hvac - off/cool/heat/fan
    * fan - on/off/auto
    * goalTemperature

--- steward/devices/devices-climate/climate-nest-control.js
+++ steward/devices/devices-climate/climate-nest-control.js
@@ -56,6 +56,83 @@ Sensor.prototype.update = function(self, params) {
   }
 };
 
+Sensor.operations = {
+  set: function(nest, params) {
+
+    // TODO: we need to associate the incoming nest with it's device ID
+    nest.serial = null;
+
+    Object.keys(params).forEach(function(key) {
+      var value = params[key];
+
+      if (key === 'away') {
+        if (value === 'on') {
+          nest.setAway(nest.serial);
+        } else {
+          nest.setHome(nest.serial);
+        }
+      }
+
+      if (key === 'hvac') {
+        switch (value) {
+          case 'off':
+          case 'cool':
+          case 'heat':
+            nest.setTargetTemperatureType(nest.serial, value);
+          break;
+
+          case 'fan':
+            nest.setFanModeOn(nest.serial);
+          break;
+        }
+      }
+
+      if (key === 'fan') {
+        switch (value) {
+
+          case 'on':
+            nest.setFanMode(nest.serial, value);
+          case 'off':
+          case 'auto':
+            nest.setFanMode(nest.serial, 'auto');
+          break;
+
+          default:
+            var time = parseInt(value);
+            if (!isNaN(time)) {
+              nest.setFanMode(nest.serial, 'duty-cycle', time);
+            }
+          break;
+        }
+      }
+
+      if (key === 'goalTemperature') {
+        nest.setTemperature(nest.serial, value);
+      }
+
+    });
+  }
+};
+
+Sensor.prototype.perform = function(self, taskID, perform, parameter) {
+  var params;
+  try { params = JSON.parse(parameter); } catch(e) {}
+
+  if (!!Sensor.operations[perform]) {
+    Sensor.operations[perform](this.gateway.nest, params);
+    return steward.performed(taskID);
+  }
+
+  return devices.perform(self, taskID, perform, parameter);
+};
+
+var validate_perform = function(perform, parameter) {
+  // TODO: check values here
+  if (!!Sensor.operations[perform]) return { invalid: [], requires: [] };
+
+  return devices.validate_perform(perform, parameter);
+};
+
 exports.start = function() {
   steward.actors.device.climate.nest = steward.actors.device.climate.nest ||
       { $info     : { type: '/device/climate/nest' } };
@@ -75,6 +152,7 @@ exports.start = function() {
                                    , leaf            : [ 'on', 'off' ]
                                    }
                     }
+        , $validate : { perform    : validate_perform }
       };
   devices.makers['/device/climate/nest/control'] = Sensor;
 };
