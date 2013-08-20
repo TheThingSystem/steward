f7cd91414ac289d2c8bf8016995fdb0a0b8d2138
Alasdair Allan <alasdair@babilim.co.uk>
2013-08-20 14:25:04 +0100

    Simple client to monitor the event stream

---
+++ clients/examples/node/monitor.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
+var util = require("util");
+var WebSocket = require('ws');
+
+var ws = new WebSocket('wss://localhost:8888/console');
+console.log("Created websocket.");
+
+ws.onopen = function(event) {
+	console.log("Opened websocket to steward.");
+
+};
+
+ws.onmessage = function(event) {
+	var str = JSON.stringify(JSON.parse(event.data), null, 2);
+	console.log("Socket message: " + str);
+
+};
+
+ws.onclose = function(event) {
+	console.log("Socket closed: " + event.wasClean );
+
+};
+
+ws.onerror = function(event) {
+	console.log("Socket error: " + util.inspect(event, {depth: null}));
+    try { 
+		ws.close (); 
+		console.log("Closed websocket.");
+	} catch (ex) {}
+};
