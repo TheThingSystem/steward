ed937c1328a2c07d000cbebe4752ba86f8b79ebe
Alasdair Allan <alasdair@babilim.co.uk>
2013-10-16 15:10:02 +0100

    Added OWL Intuition-c to drilldown

--- steward/sandbox/d3/drilldown.js
+++ steward/sandbox/d3/drilldown.js
@@ -1680,6 +1680,13 @@ var entries = {
                                                               , instrux : single_thermostat_instructions
                                                               , pop     : 'thermostat_pop'
                                                               }
+															
+			, '/device/climate/owl/monitor'              : { img     : 'actors/owl-monitor.svg'
+														   , single  : single_thermostat_drilldown
+														   , arcs    : climate_thermostat_arcs
+														   , instrux : single_device_instructions
+														 }											
+															
               , '/device/climate/netatmo/sensor'            : { img     : 'actors/netatmo.svg'
                                                               , single  : single_climate_drilldown
                                                               , arcs    : climate_device_arcs
