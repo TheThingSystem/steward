95062477a738262f69ad1cfd6b94a4887e8021b2
Marshall Rose <mrose17@gmail.com>
2013-12-17 15:35:18 -0800

    and get serialNumber...

--- steward/devices/devices-gateway/gateway-rfxcom-usb.js
+++ steward/devices/devices-gateway/gateway-rfxcom-usb.js
@@ -211,6 +211,8 @@ var scan = function() {
           info[i].modelName = fingerprints[j].modelName;
           info[i].description = fingerprints[j].description;
           info[i].deviceType = fingerprints[j].deviceType;
+          if (!info[i].manufacturer) info[i].manufacturer = fingerprints[j].manufacturer;
+          if (!info[i].serialNumber) info[i].serialNumber = info[i].pnpId.substr(fingerprints[j].pnpId.length).split('-')[0];
           scan1(info[i]);
         }
       }
