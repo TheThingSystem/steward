1265b8293660b66085c27b0ee5ce3c49b0c31354
Marshall Rose <mrose17@gmail.com>
2014-02-03 01:00:37 -0800

    Xcode CLI nonsense...

--- scripts/bootstrap-mac.sh
+++ scripts/bootstrap-mac.sh
@@ -5,11 +5,17 @@ if [ ! -x scripts/bootstrap-mac.sh ]; then
   exit 1
 fi
 
+if [ ! -d /Applications/Xcode.app ]; then
+  echo "Xcode must be fully installed, not just the command-line tools" 1>&2
+  exit 1
+fi
+
 
 echo "Step 1. Install/Update homebrew and libraries."
 
 if [ ! -d /usr/local/Cellar ]; then
   echo "Please press RETURN if asked to continue"
+  echo "If asked to install command line tools, please click 'Not Now' and then RETURN here"
 
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
