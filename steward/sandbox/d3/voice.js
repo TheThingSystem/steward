070f552d3af5dc26f3590083eb937a266e3f4056
ae9f <dannyg@dannyg.com>
2014-03-27 23:26:36 -0700

    HTML5/D3: More Curator-only preflight testing

--- steward/sandbox/d3/voice.js
+++ steward/sandbox/d3/voice.js
@@ -370,13 +370,9 @@ All recognized commands without filter\n\
     };
 
     for (i = 0; i < voiceEntries.groups.length; i++) {
-      entry = voiceEntries.groups[i].entry;
-      if (entry.type === 'task') perform3(entry.uuid, entry.name);
+      entry = voiceEntries.groups[i];
+      if (entry.phrases['perform'].selected === true && entry.entry.type === 'task') perform3(entry.entry.uuid, entry.phrases['perform'].text);
     }
-//     for (groupID in groups) {
-//       if ((groups.hasOwnProperty(groupID)) || (groups[groupID].type === 'task')) perform3(groupID, groups[groupID].name);
-//     }
-
 
     var perform4 = function(taskID, text) {
       partN++;
@@ -390,10 +386,9 @@ All recognized commands without filter\n\
     };
 
     for (i = 0; i < voiceEntries.tasks.length; i++) {
-      entry = voiceEntries.tasks[i].entry;
-      perform4(entry.uuid, entry.name);
+      entry = voiceEntries.tasks[i];
+      if (entry.phrases['perform'].selected === true) perform4(entry.entry.uuid, entry.phrases['perform'].text);
     }
-//    for (taskID in tasks) if (tasks.hasOwnProperty(taskID)) perform4(taskID, tasks[taskID].name);
 
 
     if (voiceEntries.recognizer === "tasker") {
@@ -446,16 +441,80 @@ All recognized commands without filter\n\
 
 
 var voiceUtils = {
-  categories : [ {name: 'motive', img: 'popovers/assets/actors/motive-vehicle.svg', active: false, top: 0, left: 0}
-               , {name: 'climate', img: 'popovers/assets/actors/sensor-climate.svg', active: false, top: 0, left: 0}
-               , {name: 'lighting', img: 'popovers/assets/actors/lighting-downlight.svg', active: false, top: 0, left: 0}
-               , {name: 'motive-lock', img: 'popovers/assets/actors/motive-lock.svg', active: false, top: 0, left: 0}
-               , {name: 'media', img: 'popovers/assets/actors/media-video.svg', active: false, top: 0, left: 0}
-               , {name: 'sensor', img: 'popovers/assets/actors/sensor-generic.svg', active: false, top: 0, left: 0}
-               , {name: 'switch', img: 'popovers/assets/actors/switch-onoff.svg', active: false, top: 0, left: 0}
-               , {name: 'groups', img: 'categories/groups.svg', active: true, top: 0, left: 0}
-               , {name: 'tasks', img: 'categories/tasks.svg', active: true, top: 0, left: 0}
+  categories : [ {name: 'motive'
+                   , img: 'popovers/assets/actors/motive-vehicle.svg'
+                   , active: false
+                   , q: ['motive_model-s', 'motive_vehicle']
+                   , top: 0
+                   , left: 0}
+                 , {name: 'climate'
+                   , img: 'popovers/assets/actors/sensor-climate.svg'
+                   , active: false
+                   , q: ['climate_control', 'climate_plant', 'climate_soil', 'climate_meteo']
+                   , top: 0
+                   , left: 0}
+                 , {name: 'lighting'
+                   , img: 'popovers/assets/actors/lighting-downlight.svg'
+                   , active: false
+                   , q: ['lighting_bulb', 'lighting_downlight', 'lighting_lightstrip', 'lighting_uplight', 'lighting_rgb']
+                   , top: 0
+                   , left: 0}
+                 , {name: 'motive-lock'
+                   , img: 'popovers/assets/actors/motive-lock.svg'
+                   , active: false
+                   , q: ['motive_lock']
+                   , top: 0
+                   , left: 0}
+                 , {name: 'media'
+                   , img: 'popovers/assets/actors/media-video.svg'
+                   , active: false
+                   , q: ['media_video']
+                   , top: 0
+                   , left: 0}
+                 , {name: 'sensor'
+                   , img: 'popovers/assets/actors/sensor-generic.svg'
+                   , active: false
+                   , q: ['sensor_co', 'sensor_co2', 'sensor_no2', 'sensor_smoke']
+                   , top: 0
+                   , left: 0}
+                 , {name: 'switch'
+                   , img: 'popovers/assets/actors/switch-onoff.svg'
+                   , active: false
+                   , q: ['switch_onoff']
+                   , top: 0
+                   , left: 0}
+                 , {name: 'groups'
+                   , img: 'categories/groups.svg'
+                   , active: true
+                   , top: 0
+                   , left: 0}
+                 , {name: 'tasks'
+                   , img: 'categories/tasks.svg'
+                   , active: true
+                   , top: 0
+                   , left: 0}
                ],
+  findCategory : function(q) {
+      return { climate_control  : 'climate'
+             , climate_plant    : 'climate'
+             , climate_soil     : 'climate'
+             , climate_meteo    : 'climate'
+             , lighting_bulb       : 'lighting'
+             , lighting_downlight  : 'lighting'
+             , lighting_lightstrip : 'lighting'
+             , lighting_uplight    : 'lighting'
+             , lighting_rgb        : 'lighting'
+             , motive_lock      : 'motive-lock'
+             , 'motive_model-s' : 'motive'
+             , motive_vehicle   : 'motive'
+             , media_video      : 'media'
+             , sensor_co        : 'sensor'
+             , sensor_co2       : 'sensor'
+             , sensor_no2       : 'sensor'
+             , sensor_smoke     : 'sensor'
+             , switch_onoff     : 'switch'
+             }[q];
+  },
   defaultDevicePhrases : function(entry, q) {
       var lighting =             { on : { text: 'turn ' + entry.name + ' on', selected: true }
                                  , off : { text: 'turn ' + entry.name + ' off', selected: true }
@@ -876,11 +935,11 @@ var voiceUtils = {
       var cat, cats, j, name, q, quad;
       quad = deviceType.split('/');
       q = quad[2] + '_' + quad[4];
-      name = (q === 'motive_lock') ? 'motive-lock' : quad[2];
+      name = voiceUtils.findCategory(q);
       cats = voiceUtils.categories;
       for (j = 0; j < cats.length; j++) {
         cat = cats[j];
-        if (cat.name === name) {
+        if (cat.name === name && cat.q.join('|').indexOf(q) != -1) {
           cat.active = (cat.name === name);
           document.getElementById(cat.name + '-voice').style.opacity = (cat.active) ? 1.0 : 0.3;
           return;
@@ -932,7 +991,7 @@ var voiceUtils = {
 var buildVoiceEntries = function(actors, groups, tasks, storedEntries, stewardID) {
   var actor, catName, catObj, catObjCatalog, deviceObj, entry, group, groupObj, i, newEntries, q, quad, task, taskObj;
   
-  newEntries = { groups : [], tasks : [], devices : [], categories : [] };
+  newEntries = { actors : actors, devices : [], categories : [], groups : [], tasks : [] };
   if (!storedEntries) {
     voiceEntries.stewardID = stewardID;
     voiceEntries.actors = actors;
@@ -947,7 +1006,7 @@ var buildVoiceEntries = function(actors, groups, tasks, storedEntries, stewardID
     entry = actors[actor];
     quad = entry.whatami.split('/');
     q = quad[2] + '_' + quad[4];
-    catName = (q === 'motive_lock') ? 'motive-lock' : quad[2];
+    catName = voiceUtils.findCategory(q);
     deviceObj = {
                  entry      : entry
                  , q        : q
@@ -958,7 +1017,7 @@ var buildVoiceEntries = function(actors, groups, tasks, storedEntries, stewardID
                 };
     newEntries.devices.push(deviceObj);
     
-    if (!catObjCatalog.hasOwnProperty(q)) {
+    if (!catObjCatalog.hasOwnProperty(q) && voiceUtils.findCategory(q)) {
       catObj = {
                  entry      : entry
                  , q        : q
@@ -1023,7 +1082,84 @@ var buildVoiceEntries = function(actors, groups, tasks, storedEntries, stewardID
   }
   
   function reconcile(newEntries, storedEntries) {
+    var cmd, devID, found, i, j, newItem, newName, oldItem, oldName, regexp;
+    
+    for (i = storedEntries.devices.length - 1; i >= 0; i--) {
+      oldItem = storedEntries.devices[i];
+      devID = oldItem.entry.whoami;
+      oldName = oldItem.entry.name;
+      found = false;
+      
+      for (j = newEntries.devices.length - 1; j >= 0; j--) {
+        newItem = newEntries.devices[j];
+        if (devID === newItem.entry.whoami) {
+          found = true;
+          newName = newItem.entry.name;
+          if (oldName !== newName) {
+            oldItem.entry = newItem.entry;
+            for (cmd in oldItem.phrases) {
+              regexp = new RegExp('\\b' + oldName + '\\b', 'i');
+              oldItem.phrases[cmd].text = oldItem.phrases[cmd].text.replace(regexp, newName);
+            }
+          }
+          newEntries.devices.splice(j, 1);
+          break;
+        }
+      }
+      if (!found) storedEntries.devices.splice(i, 1);
+    }
+    if (newEntries.devices.length > 0) storedEntries.devices = storedEntries.devices.concat(newEntries.devices);
+    
+    for (i = storedEntries.categories.length - 1; i >= 0; i--) {
+      oldItem = storedEntries.categories[i];
+      found = false;
+      
+      for (j = newEntries.categories.length - 1; j >= 0; j--) {
+        newItem = newEntries.categories[j];
+        if (oldItem.category === newItem.category && oldItem.q === newItem.q) {
+          found = true;
+          newEntries.categories.splice(j, 1);
+          break;
+        }
+      }
+      if (!found) storedEntries.categories.splice(i, 1);
+    }
+    if (newEntries.categories.length > 0) storedEntries.categories = storedEntries.categories.concat(newEntries.categories);
+    
+    for (i = storedEntries.groups.length - 1; i >= 0; i--) {
+      oldItem = storedEntries.groups[i];
+      found = false;
+      
+      for (j = newEntries.groups.length - 1; j >= 0; j--) {
+        newItem = newEntries.groups[j];
+        if (oldItem.entry.uuid === newItem.entry.uuid) {
+          found = true;
+          newEntries.groups.splice(j, 1);
+          break;
+        }
+      }
+      if (!found) storedEntries.groups.splice(i, 1);
+    }
+    if (newEntries.groups.length > 0) storedEntries.groups = storedEntries.groups.concat(newEntries.groups);
+        
+    for (i = storedEntries.tasks.length - 1; i >= 0; i--) {
+      oldItem = storedEntries.tasks[i];
+      found = false;
+      
+      for (j = newEntries.tasks.length - 1; j >= 0; j--) {
+        newItem = newEntries.tasks[j];
+        if (oldItem.entry.uuid === newItem.entry.uuid) {
+          found = true;
+          newEntries.tasks.splice(j, 1);
+          break;
+        }
+      }
+      if (!found) storedEntries.tasks.splice(i, 1);
+    }
+    if (newEntries.tasks.length > 0) storedEntries.tasks = storedEntries.tasks.concat(newEntries.tasks);
+
     voiceEntries = storedEntries;
+    voiceUtils.saveVoiceEntries();
   }
 }
 
