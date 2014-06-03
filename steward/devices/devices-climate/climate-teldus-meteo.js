dd740d631ee318877f6386ed849ff5819f3096fb
Björn Torkelsson <torkel@torkel.se>
2014-06-04 00:22:15 +0200

    Sync with climate-oregon

--- steward/devices/devices-climate/climate-teldus-meteo.js
+++ steward/devices/devices-climate/climate-teldus-meteo.js
@@ -61,16 +61,57 @@ exports.start = function() {
                     , properties : { name         : true
                                    , status       : [ 'present' ]
                                    , lastSample   : 'timestamp'
+                                   , temperature   : 'celsius'
+                                   , humidity      : 'percentage'
                                    }
                     }
       , $validate : { perform    : devices.validate_perform
                     }
       };
+
+  steward.actors.device.climate.telldus.rain =
+      { $info     : { type       : '/device/climate/telldus/rain'
+                    , observe    : [ ]
+                    , perform    : [ ]
+                    , properties : { name         : true
+                                   , status       : [ 'present' ]
+                                   , lastSample   : 'timestamp'
+                                   , rainRate      : 'mm/h'
+                                   , rainTotal     : 'mm'
+                                   }
+                    }
+      , $validate : { perform    : devices.validate_perform
+                    }
+      };
+  steward.actors.device.climate.telldus.wind =
+      { $info     : { type       : '/device/climate/telldus/wind'
+                    , observe    : [ ]
+                    , perform    : [ ]
+                    , properties : { name         : true
+                                   , status       : [ 'present' ]
+                                   , lastSample   : 'timestamp'
+                                   , windAverage   : 'm/s'
+                                   , windGust      : 'm/s'
+                                   , windDirection : 'degrees'
+                                   }
+                    }
+      , $validate : { perform    : devices.validate_perform
+                    }
+      };
+
   devices.makers['/device/climate/telldus/meteo'] = Sensor;
+  devices.makers['/device/climate/telldus/rain'] = Sensor;
+  devices.makers['/device/climate/telldus/wind'] = Sensor;
 
   steward.actors.device.climate.fineoffset = steward.actors.device.climate.fineoffset ||
       { $info: { type: '/device/climate/fineoffset' } };
   steward.actors.device.climate.fineoffset.meteo = utility.clone(steward.actors.device.climate.telldus.meteo);
-  steward.actors.device.climate.fineoffset.meteo.$info.type = '/device/climate/fineoffset/meteo';
+  steward.actors.device.climate.fineoffset.meteo.$info.type = '/device/climate/fineoffset/meteo'
   devices.makers['/device/climate/fineoffset/meteo'] = Sensor;
+  steward.actors.device.climate.fineoffset.rain = utility.clone(steward.actors.device.climate.telldus.rain);
+  steward.actors.device.climate.fineoffset.rain.$info.type = '/device/climate/fineoffset/rain'
+  devices.makers['/device/climate/fineoffset/rain'] = Sensor;
+  steward.actors.device.climate.fineoffset.wind = utility.clone(steward.actors.device.climate.telldus.wind);
+  steward.actors.device.climate.fineoffset.wind.$info.type = '/device/climate/fineoffset/wind'
+  devices.makers['/device/climate/fineoffset/wind'] = Sensor;
 };
