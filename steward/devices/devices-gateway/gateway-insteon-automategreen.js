b0cfb8c2cf2764ab4ca521bc2b5db69abcc75252
Marshall Rose <mrose17@gmail.com>
2014-06-06 11:09:56 -0400

    in anticipation...

--- steward/devices/devices-gateway/gateway-insteon-automategreen.js
+++ steward/devices/devices-gateway/gateway-insteon-automategreen.js
@@ -101,7 +101,7 @@ Gateway.prototype.scan = function(self) {
       };
 
       for (i = 0; i < links.length; i++) {
-        if (typeof links[i] === 'undefined') continue;
+        if (typeof links[i] === 'undefined' || typeof links[i] === null) continue;
         id = links[i].id;
 
         if (!!self.seen[id]) continue;
