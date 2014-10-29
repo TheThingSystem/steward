5a27a176b39d6a6cedf7a9ff6a12bc8f8d2cb9c2
ebartel <ebartel@192.168.88.202>
2014-10-28 21:29:53 -0400

    Fix Indents

--- steward/devices/devices-sensor/sensor-zwave-multilevel.js
+++ steward/devices/devices-sensor/sensor-zwave-multilevel.js
@@ -1,189 +1,204 @@
 // Z-wave multi-level sensor
 
 var registrar
-				, utility = require('./../../core/utility')
-				;
+        , utility = require('./../../core/utility')
+        ;
 
 try {
-	registrar = require('./../devices-gateway/gateway-openzwave-usb');
-	if (!registrar.pair)
-		throw new Error('openzwave-usb gateway unable to start');
+  registrar = require('./../devices-gateway/gateway-openzwave-usb');
+  if (!registrar.pair)
+    throw new Error('openzwave-usb gateway unable to start');
 } catch (ex) {
-	exports.start = function() {
-	};
+  exports.start = function() {
+  };
 
-	return utility.logger('devices').info('failing zwave-multi sensor (continuing)', {diagnostic: ex.message});
+  return utility.logger('devices').info('failing zwave-multi sensor (continuing)', {diagnostic: ex.message});
 }
 
 var util = require('util')
-				, devices = require('./../../core/device')
-				, steward = require('./../../core/steward')
-				, sensor = require('./../device-sensor')
-				;
+        , devices = require('./../../core/device')
+        , steward = require('./../../core/steward')
+        , sensor = require('./../device-sensor')
+        ;
 
 
 var logger = sensor.logger;
 
 
 var ZWave_MLSensor = exports.Device = function(deviceID, deviceUID, info) {
-	var self = this;
+  var self = this;
 
-	self.whatami = info.deviceType;
-	self.deviceID = deviceID.toString();
-	self.deviceUID = deviceUID;
-	self.name = info.device.name;
-	self.getName();
+  self.whatami = info.deviceType;
+  self.deviceID = deviceID.toString();
+  self.deviceUID = deviceUID;
+  self.name = info.device.name;
+  self.getName();
 
-	// Command Classes 
-	//  Multi-Level Sensor: 0x31
-	//  Battery: 0x80
+  // Command Classes 
+  //  Multi-Level Sensor: 0x31
+  //  Battery: 0x80
 
-	self.status = 'present';
+  self.status = 'present';
 
-	self.changed();
-	self.driver = info.driver;
-	self.peripheral = info.peripheral;
-	self.info = {
-		temperature: (typeof info.peripheral.classes[0x31][1].value === 'undefined') ? 0 : info.peripheral.classes[0x31][1].value,
-		light: (typeof info.peripheral.classes[0x31][3].value === 'undefined') ? 0 : info.peripheral.classes[0x31][3].value,
-		humidity: (typeof info.peripheral.classes[0x31][5].value === 'undefined') ? 0 : info.peripheral.classes[0x31][5].value,
-		battery: (typeof info.peripheral.classes[0x80][0].value === 'undefined') ? 0 : info.peripheral.classes[0x80][0].value
-	};
+  self.changed();
+  self.driver = info.driver;
+  self.peripheral = info.peripheral;
+  self.info = {
+    temperature: (typeof info.peripheral.classes[0x31][1].value === 'undefined') ? 0 : info.peripheral.classes[0x31][1].value,
+    light: (typeof info.peripheral.classes[0x31][3].value === 'undefined') ? 0 : info.peripheral.classes[0x31][3].value,
+    humidity: (typeof info.peripheral.classes[0x31][5].value === 'undefined') ? 0 : info.peripheral.classes[0x31][5].value,
+    batteryLevel: (typeof info.peripheral.classes[0x80][0].value === 'undefined') ? 0 : info.peripheral.classes[0x80][0].value
+  };
 
-	utility.broker.subscribe('actors', function(request, taskID, actor, perform, parameter) {
-		if (actor !== ('device/' + self.deviceID))
-			return;
+  utility.broker.subscribe('actors', function(request, taskID, actor, perform, parameter) {
+    if (actor !== ('device/' + self.deviceID))
+      return;
 
-		if (request === 'perform')
-			return self.perform(self, taskID, perform, parameter);
-	});
+    if (request === 'perform')
+      return self.perform(self, taskID, perform, parameter);
+  });
 
-	self.driver.setValue(self.peripheral.nodeid, 0x31, 101, 5);
-	self.driver.setValue(self.peripheral.nodeid, 0x31, 111, 1);
-	//self.driver.enablePoll(self.peripheral.nodeid, 0x31);
 };
 util.inherits(ZWave_MLSensor, sensor.Device);
 
 
 ZWave_MLSensor.prototype.update = function(self, event, comclass, value) {
-	if (event === 'value added')
-		event = 'value changed';
+  if (event === 'value added')
+    event = 'value changed';
 
-	var f = {'value changed':
-						function() {
+  var f = {'value changed':
+            function() {
 
-							if (!self.peripheral.classes[comclass])
-								self.peripheral.classes[comclass] = {};
-							self.peripheral.classes[comclass][value.index] = value;
-							if ((comclass !== 0x31))
-								return;
+              if (!self.peripheral.classes[comclass])
+                self.peripheral.classes[comclass] = {};
+              self.peripheral.classes[comclass][value.index] = value;
+              if ((comclass !== 0x31))
+                return;
 
-							// TBD: could do something more with this
-							self.status = 'present';
-							switch (comclass) {
-								case 0x31:
-									switch (value.index) {
-										case 1:
-											self.info.temperature = value.value;
-											break;
-										case 3:
-											self.info.light = value.value;
-											break;
-										case 5:
-											self.info.measurement = value.value;
-											break;
-										default:
-											logger.warning('device/' + self.deviceID, {event: 'value changed', description: 'Multi-Sensor value not supported of \'' + value.label + '\' (index: ' + value.index + ')'});
-											console.log(util.inspect(value));
-											return;
-									}
-									break;
-								case 0x80:
-									switch (value.index) {
-										case 0:
-											self.info.battery = value.value;
-											break;
-										default:
-											logger.warning('device/' + self.deviceID, {event: 'value changed', description: 'Battery value not supported of \'' + value.label + '\' (index: ' + value.index + ')'});
-											console.log(util.inspect(value));
-											return;
-									}
-									break;
-								default:
-									logger.warning('device/' + self.deviceID, {event: 'value changed', description: 'Command Class not supported for \'' + comclass + '\' (index: ' + value.index + ')'});
-									console.log(util.inspect(value));
-									return;
-							}
-							logger.info('device/' + self.deviceID, {event: 'value changed', command_class: comclass, label: value.label, value: value.value, index: value.index});
-							self.info.lastSample = new Date().getTime();
-							self.changed();
-							sensor.update(self.deviceID, {lastSample: self.info.lastSample
-								, temperature: self.info.temperature
-								, light: self.info.light
-								, humidity: self.info.humidity
-								, battery: self.info.battery
-							});
-						}
+              // TBD: could do something more with this
+              self.status = 'present';
+              switch (comclass) {
+                case 0x31:
+                  switch (value.index) {
+                    case 1:
+                      // Take care of bug in Aeon firmware
+                      if (value.value < 1000 && value.value > -1000)
+                        self.info.temperature = Math.round((value.value - 32) * 5 / 9);
+                      break;
+                    case 3:
+                      self.info.light = value.value;
+                      break;
+                    case 5:
+                      self.info.measurement = value.value;
+                      break;
+                    default:
+                      logger.warning('device/' + self.deviceID, {event: 'value changed', description: 'Multi-Sensor value not supported of \'' + value.label + '\' (index: ' + value.index + ')'});
+                      console.log(util.inspect(value));
+                      return;
+                  }
+                  break;
+                case 0x80:
+                  switch (value.index) {
+                    case 0:
+                      self.info.batteryLevel = value.value;
+                      break;
+                    default:
+                      logger.warning('device/' + self.deviceID, {event: 'value changed', description: 'Battery value not supported of \'' + value.label + '\' (index: ' + value.index + ')'});
+                      console.log(util.inspect(value));
+                      return;
+                  }
+                  break;
+                default:
+                  logger.warning('device/' + self.deviceID, {event: 'value changed', description: 'Command Class not supported for \'' + comclass + '\' (index: ' + value.index + ')'});
+                  console.log(util.inspect(value));
+                  return;
+              }
+              logger.info('device/' + self.deviceID, {event: 'value changed', command_class: comclass, label: value.label, value: value.value, index: value.index});
+              self.info.lastSample = new Date().getTime();
+              self.changed();
+              sensor.update(self.deviceID, {lastSample: self.info.lastSample
+                , temperature: self.info.temperature
+                , light: self.info.light
+                , humidity: self.info.humidity
+                , batteryLevel: self.info.batteryLevel
+              });
+            }
 
-		, 'value removed':
-						function() {
-							try {
-								delete(self.peripheral.classes[comclass][value]);
-							} catch (ex) {
-							}
-
-// TBD: something to do here?
-						}
-		, 'notification':
-						function() {
-							logger.warning('device/' + self.deviceID, {event: 'notification', value: value});
+    , 'value removed':
+            function() {
+              try {
+                delete(self.peripheral.classes[comclass][value]);
+              } catch (ex) {
+              }
 
 // TBD: something to do here?
-						}
-	};
-	if (!!f[event])
-		return (f[event])();
+            }
+    , 'notification':
+            function() {
+              logger.warning('device/' + self.deviceID, {event: 'notification', value: value});
 
-	logger.warning('device/' + self.deviceID,
-					{event: event, comclass: comclass, value: value, diagnostic: 'unexpected update'});
+// TBD: something to do here?
+            }
+  };
+  if (!!f[event])
+    return (f[event])();
+
+  logger.warning('device/' + self.deviceID,
+          {event: event, comclass: comclass, value: value, diagnostic: 'unexpected update'});
 };
 
 
 ZWave_MLSensor.prototype.perform = function(self, taskID, perform, parameter) {
-	var params, state;
+  var params, state;
+
+  try {
+    params = JSON.parse(parameter);
+  } catch (ex) {
+    params = {};
+  }
+
+  if (perform === 'set') {
+    if (!!params.name)
+      self.driver.setName(self.peripheral.nodeid, params.name);
+    if (!!params.physical)
+      self.driver.setLocation(self.peripheral.nodeid, params.physical);
+
+    return (devices.perform(self, taskID, perform, parameter) || (!!params.physical));
+  }
 
 //  TBD: Add perform for configuring device once SetConfigParam available through node-openzwave
 };
 
 
 var validate_perform = function(perform, parameter) {
-	var params = {}
-	, result = {invalid: [], requires: []}
-	;
+  var params = {}
+  , result = {invalid: [], requires: []}
+  ;
 
-	return result;
+  return result;
 };
 
 exports.start = function() {
-	steward.actors.device['sensor'].zwave = steward.actors.device['sensor'].zwave ||
-					{$info: {type: '/device/sensor/zwave'}};
+  steward.actors.device['sensor'].zwave = steward.actors.device['sensor'].zwave ||
+          {$info: {type: '/device/sensor/zwave'}};
 
-	steward.actors.device['sensor'].zwave.multilevel =
-					{$info: {type: '/device/sensor/zwave/multilevel'
-							, observe: []
-							, perform: []
-							, properties: {name: true
-								, status: ['present', 'absent', 'recent']
-								, lastSample: 'timestamp'
-								, temperature: 'celsius'
-								, humidity: 'percentage'
-								, light: 'lux'
-								, physical: true
-								, battery: 'percentage'
-							}
-						}
-						, $validate: {perform: validate_perform}
-					};
-	devices.makers['/device/sensor/zwave/multilevel'] = ZWave_MLSensor;
-	registrar.pair(0x31, '/device/sensor/zwave/multilevel'); // COMMAND_CLASS_SENSOR_MULTILEVEL
+  steward.actors.device['sensor'].zwave.multilevel =
+          {$info: {type: '/device/sensor/zwave/multilevel'
+              , observe: []
+              , perform: []
+              , properties: {
+                name: true
+                , status: ['present', 'absent', 'recent']
+                , lastSample: 'timestamp'
+                , temperature: 'celsius'
+                , humidity: 'percentage'
+                , light: 'lux'
+                , physical: true
+                , batteryLevel: 'percentage'
+              }
+            }
+            , $validate: {perform: validate_perform}
+          };
+  devices.makers['/device/sensor/zwave/multilevel'] = ZWave_MLSensor;
+  registrar.pair(0x31, '/device/sensor/zwave/multilevel'); // COMMAND_CLASS_SENSOR_MULTILEVEL
 };
